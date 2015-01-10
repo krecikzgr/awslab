4002b7b5de5103a7f6bdfb242fc23d96f4e8ed09
krecikzgr <kaleciarz@gmail.com>
2015-01-10 12:35:53 +0100

    test24

--- ak_lab2.js
+++ ak_lab2.js
@@ -1,2 +1,24 @@
 ﻿//var AWS = require("/node_modules/aws-sdk");
-//AWS.config.loadFromPath("./config.json");
+//AWS.config.loadFromPath("./config.json");var AWS = require('aws-sdk');
+
+AWS.config.loadFromPath('./config.json');
+
+var task = function (request, callback) {
+    var params = {
+        DryRun: true,
+
+    };
+    ec2.describeInstances(params, function (err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            callback(err, null);
+        }// an error occurred
+        else {
+            console.log(data);
+            callback(null, data);
+        }         // successful response
+    });
+
+}
+
+exports.lab = task
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var lab1_1 = require("./lab/lab1_1").lab
 var example_1 = require("./example_1").lab;
-var ak_lab2=require("./ak_lab2").lab;
+//var ak_lab2=require("./ak_lab2").lab;
 
 var PORT = 8080;
 
@@ -9,7 +9,7 @@ var urlMap = [
 	{path: "/", action:__dirname + "/static/index.html"},	 
 	{path: "/digest", action: lab1_1},	
 	{ path: "/example_1", action: example_1 },
-    {path: "/information",action:ak_lab2},
+    //{path: "/information",action:ak_lab2},
 	];
 
 var service = require("./lib/service").http(urlMap);
