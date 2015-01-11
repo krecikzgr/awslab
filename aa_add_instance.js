246647e467496d8a9f93b800792a1abec5e8d55e
krecikzgr <kaleciarz@gmail.com>
2015-01-11 09:47:46 +0100

    lab2 add instance 5

--- aa_add_instance.js
+++ aa_add_instance.js
@@ -5,12 +5,38 @@
 
 AWS.config.loadFromPath('./config.json');
 
+
+
+
+
 var task = function (request, callback) {
     var params = {
-        DryRun: false,
+        ImageId: 'i-8c249b80', /* required */
+        MaxCount: 1, /* required */
+        MinCount: 1, /* required */
+
+
+        Monitoring: {
+            Enabled: true /* required */
+        },
+        NetworkInterfaces: [
+          {
+              DeviceIndex: 0,
+              PrivateIpAddresses: [
+                {
+                    PrivateIpAddress: '172.31.28.83', /* required */
+                   
+                },
+                /* more items */
+              ],
+              SecondaryPrivateIpAddressCount: 0,
+   
+          },
+          /* more items */
+        ],
+      
     };
-    var ec2 = new AWS.EC2();
-    ec2.describeInstances(params, function (err, data) {
+    ec2.runInstances(params, function (err, data) {
         if (err) {
             console.log(err, err.stack);
             callback(err, null);
@@ -18,7 +44,7 @@ var task = function (request, callback) {
         else {
             console.log(data);
             callback(null, data);
-        }         // successful response
+        }            // successful response
     });
 
 }
