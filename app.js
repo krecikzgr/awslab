0868f7bd6067c05db3932361781a612ed1b5205d
krecikzgr <kaleciarz@gmail.com>
2015-01-11 09:29:12 +0100

    lab2 addinstance2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var lab1_1 = require("./lab/lab1_1").lab
 var example_1 = require("./example_1").lab;
 var ak_lab2 = require("./ak_lab2").lab;
-var ak_lab2_add_instance=require("./lab/lab2/aa_add_instance")
+var ak_lab2_add_instance = require("./lab/lab2/aa_add_instance").lab;
 
 var PORT = 8080;
 var urlMap = [
