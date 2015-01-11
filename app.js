c1a81a1b12dd7e3ce73ae544d7cd6c381a07a95b
krecikzgr <kaleciarz@gmail.com>
2015-01-11 09:27:05 +0100

    lab2 addinstance

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var lab1_1 = require("./lab/lab1_1").lab
 var example_1 = require("./example_1").lab;
-var ak_lab2=require("./ak_lab2").lab;
+var ak_lab2 = require("./ak_lab2").lab;
+var ak_lab2_add_instance=require("./lab/lab2/aa_add_instance")
 
 var PORT = 8080;
 var urlMap = [
 	{path: "/", action:__dirname + "/static/index.html"},	 
 	{path: "/digest", action: lab1_1},	
 	{ path: "/example_1", action: example_1 },
-    {path: "/something", action: ak_lab2},
+    { path: "/something", action: ak_lab2 },
+    {path: "/lab2addinstance", action: ak_lab2_add_instance},
+
 	];
 var service = require("./lib/service").http(urlMap);
 service(PORT);
---
+++ lab/lab2/aa_add_intance.js
@@ -0,0 +1,26 @@
+﻿
+
+//var AWS = require("/node_modules/aws-sdk");
+//AWS.config.loadFromPath("./config.json");var AWS = require("./node_modules/aws-sdk");
+
+AWS.config.loadFromPath('./config.json');
+
+var task = function (request, callback) {
+    var params = {
+        DryRun: false,
+    };
+    var ec2 = new AWS.EC2();
+    ec2.describeInstances(params, function (err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            callback(err, null);
+        }// an error occurred
+        else {
+            console.log(data);
+            callback(null, data);
+        }         // successful response
+    });
+
+}
+
+exports.lab = task
